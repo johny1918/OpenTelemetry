f16118383ea64f08b87e877b52f5dcfae85a8835
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-11-30 21:29:00 +0200

    Create custom errors and User struct

---
+++ src/error/mod.rs
@@ -0,0 +1,29 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+pub enum AppError {
+    UserNotFound,
+    UserAlreadyExists,
+    InternalServerError,
+    ValidationError(String),
+    NotFound,
+    Unauthorized,
+    Forbidden,
+    Unknown(String),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let body = match self {
+            AppError::UserNotFound => "User not found".to_string(),
+            AppError::UserAlreadyExists => "User already exists".to_string(),
+            AppError::InternalServerError => "Internal server error".to_string(),
+            AppError::ValidationError(msg) => format!("Validation error: {}", msg),
+            AppError::NotFound => "Not found".to_string(),
+            AppError::Unauthorized => "Unauthorized".to_string(),
+            AppError::Forbidden => "Forbidden".to_string(),
+            AppError::Unknown(msg) => format!("Unknown error, all I got is this: {}", msg),
+        };
+            (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+    }
+}
---
+++ src/models/User.rs
@@ -0,0 +1,8 @@
+use uuid::Uuid;
+
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub name: String,
+    pub timestamp: chrono::DateTime<chrono::Utc>
+}
